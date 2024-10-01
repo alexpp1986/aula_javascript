6988b6bf06a697cfc530fc65bc5bcea3c8308ce2
alexpp1986 <alexpp.pinto@gmail.com>
2024-09-30 22:34:31 -0300

    codigo de soma e subtracao

---
+++ calculo.js
@@ -0,0 +1,24 @@
+
+//opção para calculo de soma e subtracao
+function soma (a, b) {
+    const result = a + b
+    console.log ("Valor da Soma:", result, "\n")
+    return result
+    }
+    const resultado = soma(2, 3);
+    
+
+    function sub (x, y) {
+        const result = x - y
+        console.log ("Valor da sub:", result,)
+        return result
+      }
+      const subtracao = sub(10, 8);
+
+function somaSubtrai(n1, n2) {
+let vamossomar = n1 + n2;
+let vamosdiminuir = n1 - n2;
+console.log("somando:", vamossomar);
+console.log("subtraido:", vamosdiminuir);
+}
+somaSubtrai(10 ,8 );
